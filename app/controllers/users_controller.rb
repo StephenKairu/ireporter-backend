fe828374a0a7d1ced4e7361e334728f5a74156ab
Stephen <skairu09@gmail.com>
2023-01-21 23:36:32 +0300

    Add show, create and update method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,44 @@
 class UsersController < ApplicationController
+    # skip_before_action :authorize, only: [:index, :show, :create, :update] 
 
     def index 
-        render json: User.all
+        users = User.all
+        render json: users, status: :ok
+    end
+
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        user = User.find_by(id: session[:user_id])
+        if user
+            render json: user, status: :ok
+        else
+            render json: { error: "Not authorized" }, status: :unauthorized
+        end
+    end
+
+    def update
+        user = User.find_by(id: params[:id])
+        if user
+            user.update(user_params)
+            render json: user
+        else
+            render json: { error: "User not found" }, status: :not_found
+        end
+    end
+
+
+    private
+
+    def user_params
+        params.permit(:username, :email, :role, :password )
     end
 end
