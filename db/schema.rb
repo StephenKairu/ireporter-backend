8861f17fede425529d6f8a4a778836de8e5e3ab2
Stephen <skairu09@gmail.com>
2023-01-23 05:03:20 +0300

    Add alerts model, associations and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_23_014845) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_23_020116) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,21 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_23_014845) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "alerts", force: :cascade do |t|
+    t.string "title"
+    t.string "message"
+    t.string "image"
+    t.float "lat"
+    t.float "lng"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "report_id"
+    t.index ["report_id"], name: "index_alerts_on_report_id"
+    t.index ["user_id"], name: "index_alerts_on_user_id"
+  end
+
   create_table "reports", force: :cascade do |t|
     t.string "report_title"
     t.string "report_message"
@@ -66,5 +81,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_23_014845) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "alerts", "reports"
+  add_foreign_key "alerts", "users"
   add_foreign_key "reports", "users"
 end
